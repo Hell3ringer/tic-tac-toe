546a7c747f1c2d13edc4462d0d7eb4139a030998
@Hell3ringer <vamshiduvva23@gmail.com>
2021-01-05 13:13:09 +0530

    on click displays "O"

--- Assets/scripts/O.cs
+++ Assets/scripts/O.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class O : MonoBehaviour
 {
+    public Sprite spo,spnull;
+    public Button btn;
+    public bool btnclked;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        btn.onClick.AddListener(delegate { changeimg(); });
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    public void changeimg()
+    {
+        if(btn.GetComponent<Image>().sprite == spnull)
+        {
+            btn.GetComponent<Image>().sprite = spo;
+        }       
+         
         
     }
 }
